b67b279edda3ea9e274442e2c387a0f53478a1cc
bigwin22 <gogletaehyung0808@naver.com>
2024-05-28 10:25:15 +0900

    prediction_analyze END

--- Prediction_Ananalyze.py
+++ Prediction_Ananalyze.py
@@ -15,7 +15,7 @@ def preprocess_data(country_data, mapping):
     processed_data = {}
     for indicator_key, values in country_data.items():
         indicator_name = mapping.get(indicator_key, indicator_key)
-        processed_data[indicator_name] = {int(year): value for year, value in values.items()}
+        processed_data[indicator_name] = {int(year): value for year, value in values.items() if value is not None}
     return processed_data
 
 # 한국 데이터 처리
@@ -34,28 +34,37 @@ def predict_and_correlate(jpn_data, kor_data):
     correlation_results = {}
     for indicator, jpn_values in jpn_data.items():
         # ARIMA 모델을 학습하고 예측
-        model = ARIMA(np.array(list(jpn_values.values())), order=(5,1,0))
-        model_fit = model.fit()
-        forecast = model_fit.forecast(steps=len(kor_data[indicator]))
-        
-        # 예측값과 한국의 현재 데이터 간의 상관관계 계산
-        kor_values = kor_data.get(indicator, {})
-        if kor_values:
-            correlation = np.corrcoef(list(kor_values.values()), forecast[:len(kor_values)])[0, 1]
-            correlation_results[indicator] = correlation
-            print(f'{indicator}의 상관계수: {correlation}')
-        else:
-            print(f'{indicator}의 한국 데이터를 찾을 수 없습니다.')
+        jpn_values_list = list(jpn_values.values())
+        if len(jpn_values_list) > 0:
+            model = ARIMA(np.array(jpn_values_list), order=(5,1,0))
+            model_fit = model.fit()
+            forecast = model_fit.forecast(steps=len(kor_data[indicator]))
+
+            # 예측값과 한국의 현재 데이터 간의 상관관계 계산
+            kor_values = kor_data.get(indicator, {})
+            if kor_values:
+                kor_values_list = list(kor_values.values())
+                # Ensure forecast and kor_values_list are of the same length
+                min_length = min(len(forecast), len(kor_values_list))
+                if min_length > 0:
+                    correlation = np.corrcoef(kor_values_list[:min_length], forecast[:min_length])[0, 1]
+                    correlation_results[indicator] = correlation
+                    print(f'{indicator}의 상관계수: {correlation}')
+                else:
+                    print(f'{indicator}의 유효한 데이터가 부족합니다.')
+            else:
+                print(f'{indicator}의 한국 데이터를 찾을 수 없습니다.')
     
     return correlation_results
 
 correlation_results = predict_and_correlate(jpn_data, kor_data)
 
-# 상관관계 시각화
+# 상관관계 시각화(이미지 저장)
 plt.barh(list(correlation_results.keys()), list(correlation_results.values()))
 plt.xlabel('상관관계')
 plt.ylabel('지표')
 plt.title('한국과 일본의 예측된 미래 데이터와의 상관관계')
 plt.grid(True, linestyle='--', linewidth=0.5)
 plt.tight_layout()
+plt.savefig('./images/prediction_correlation_results.png')
 plt.show()
